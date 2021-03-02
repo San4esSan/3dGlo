e1a7cf2653e16ddf3cee258ef8f958ee9b0af37f
San4es <mister.San4es@list.ru>
2021-03-02 17:49:35 +0400

    lesson18 deb

--- script/script.js
+++ script/script.js
@@ -13,46 +13,44 @@ window.addEventListener('DOMContentLoaded', function () {
 
       let timeRemaining = (dateStop - dateNow) / 1000,
         seconds = Math.floor(timeRemaining % 60),
+        
         minutes = Math.floor((timeRemaining / 60) % 60),
         hours = Math.floor(timeRemaining / 60 / 60) % 24,
         day = Math.floor(timeRemaining / 60 / 60 / 24);
-
+        
       if (dateStop >= dateNow) {
         return { timeRemaining, hours, minutes, seconds };
       } else {
         return {
-          seconds: '00',
-          minutes: '00',
-          hours: '00',
-          day: '00'
+          seconds: '0',
+          minutes: '0',
+          hours: '0',
+          day: '0'
         }
       }
+      console.log('seconds: ', seconds);
     }
-
     function updateClock() {
       let timer = getTimeRemaining();
       if (timer.hours < 10) {
         timerHours.textContent = '0' + timer.hours;
-      }else {
+      } else {
         timerHours.textContent = timer.hours;
       }
       if (timer.minutes < 10) {
         timerMinutes.textContent = '0' + timer.minutes;
-      }else {
+      } else {
         timerMinutes.textContent = timer.minutes;
       }
       if (timer.seconds < 10) {
         timerSeconds.textContent = '0' + timer.seconds;
-      }else {
+      } else {
         timerSeconds.textContent = timer.seconds;
       }
 
-      if (timer.timeRemaining > 0) {
-        // setTimeout(updateClock, 1000);
-        setInterval(updateClock, 1000);
-      }
     }
-    updateClock();
+    setInterval(updateClock, 1000);
   }
-  countTimer('02 march 2021');
+  // countTimer('03 march 2021');
+  countTimer('02 march 2021 20:47');
 });
